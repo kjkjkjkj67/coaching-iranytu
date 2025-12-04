ceb4c2aed409c264d5f058e235e0d95611305cd7
kjkjkjkj67 <kiss.jozsef@hdteam.hu>
2025-12-04 14:36:10 +0100

    Javitas es ujrainditas

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    // Ez szükséges ahhoz, hogy a process.env.API_KEY működjön a böngészőben is
-    'process.env': process.env
+export default defineConfig(({ mode }) => {
+  // Betöltjük a környezeti változókat (pl. API_KEY a Vercelről)
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      // Ez teszi lehetővé, hogy a kódban a process.env.API_KEY működjön
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    }
   }
 })
