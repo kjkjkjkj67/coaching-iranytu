6eb8054c944810d9b9d5ed09991a56c353aa12eb
kjkjkjkj67 <kiss.jozsef@hdteam.hu>
2025-12-04 14:48:34 +0100

    Fajlok javitasa kezzel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Betöltjük a környezeti változókat (pl. API_KEY a Vercelről)
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
   
   return {
     plugins: [react()],
